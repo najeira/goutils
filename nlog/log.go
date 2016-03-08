d2a2c01ffd2e4d5f8f4ed5bea8b7a785b2066fd1
najeira <najeira@gmail.com>
2016-03-08 20:07:18 +0900

    update

--- nlog/log.go
+++ nlog/log.go
@@ -10,13 +10,12 @@ import (
 
 const (
 	No = iota
-	Critical
+	Fatal
 	Error
 	Warn
-	Notice
 	Info
 	Debug
-	Verbose
+	Trace
 )
 
 const (
@@ -29,26 +28,22 @@ const (
 )
 
 var levelName = map[int]string{
-	Critical: "CRITICAL",
-	Error:    "ERROR",
-	Warn:     "WARN",
-	Notice:   "NOTICE",
-	Info:     "INFO",
-	Debug:    "DEBUG",
-	Verbose:  "VERBOSE",
+	Fatal: "FATAL",
+	Error: "ERROR",
+	Warn:  "WARN",
+	Info:  "INFO",
+	Debug: "DEBUG",
+	Trace: "TRACE",
 }
 
 type Logger interface {
-	SetLevel(level int)
-	SetLevelName(level string)
-	Enable(level int) bool
-	Verbosef(format string, v ...interface{})
+	V(level int) bool
+	Tracef(format string, v ...interface{})
 	Debugf(format string, v ...interface{})
 	Infof(format string, v ...interface{})
-	Noticef(format string, v ...interface{})
 	Warnf(format string, v ...interface{})
 	Errorf(format string, v ...interface{})
-	Criticalf(format string, v ...interface{})
+	Fatalf(format string, v ...interface{})
 }
 
 type Config struct {
@@ -86,8 +81,7 @@ func NewLogger(config *Config) Logger {
 }
 
 func LevelToName(level int) string {
-	name, ok := levelName[level]
-	if ok {
+	if name, ok := levelName[level]; ok {
 		return name
 	}
 	return ""
@@ -103,20 +97,12 @@ func NameToLevel(name string) int {
 	return No
 }
 
-func (l *myLogger) SetLevel(level int) {
-	l.level = level
-}
-
-func (l *myLogger) SetLevelName(level string) {
-	l.level = NameToLevel(level)
-}
-
-func (l *myLogger) Enable(level int) bool {
+func (l *myLogger) V(level int) bool {
 	return level <= l.level && level != No
 }
 
-func (l *myLogger) Verbosef(format string, v ...interface{}) {
-	l.Printf(Verbose, format, v...)
+func (l *myLogger) Tracef(format string, v ...interface{}) {
+	l.Printf(Trace, format, v...)
 }
 
 func (l *myLogger) Debugf(format string, v ...interface{}) {
@@ -127,10 +113,6 @@ func (l *myLogger) Infof(format string, v ...interface{}) {
 	l.Printf(Info, format, v...)
 }
 
-func (l *myLogger) Noticef(format string, v ...interface{}) {
-	l.Printf(Notice, format, v...)
-}
-
 func (l *myLogger) Warnf(format string, v ...interface{}) {
 	l.Printf(Warn, format, v...)
 }
@@ -139,16 +121,16 @@ func (l *myLogger) Errorf(format string, v ...interface{}) {
 	l.Printf(Error, format, v...)
 }
 
-func (l *myLogger) Criticalf(format string, v ...interface{}) {
-	l.Printf(Critical, format, v...)
+func (l *myLogger) Fatalf(format string, v ...interface{}) {
+	l.Printf(Fatal, format, v...)
+	os.Exit(1)
 }
 
 func (l *myLogger) Printf(level int, format string, v ...interface{}) {
 	if level > l.level {
 		return
 	}
-	name := LevelToName(level)
-	if name != "" {
+	if name := LevelToName(level); name != "" {
 		format = fmt.Sprintf("[%s] %s", name, format)
 		l.logger.Printf(format, v...)
 	}
